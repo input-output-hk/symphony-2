ee526b48f66c548dda597e00e4a50b6f998f419d
Scott Darby <darbyscott@gmail.com>
2019-07-05 17:30:33 +0100

    holding page

--- src/App.js
+++ src/App.js
@@ -4355,14 +4355,14 @@ class App extends mixin(EventEmitter, Component) {
         <div className={className}>
           <div className='landing-bg' />
           <div className='logo-container'>
-            <img className='symphony-logo' src={logo} alt='Symphony Logo' onClick={() => { this.initStage('high') }} />
+            <img className='symphony-logo' src={logo} alt='Symphony Logo' />
+            {/* <img className='symphony-logo' src={logo} alt='Symphony Logo' onClick={() => { this.initStage('high') }} /> */}
             <h1>Symphony</h1>
             <h2>3D Blockchain Explorer</h2>
-            <p className='headphone-hint'>Headphones<br />Recommended</p>
-            <i className='fas fa-headphones-alt' />
-            <p className='choose-quality'>Choose Quality:</p>
+            <p className='choose-quality'>COMING SOON...</p>
+            {/* <p className='choose-quality'>Choose Quality:</p>
             <a className='quality-select' onClick={() => { this.initStage('high') }} tooltip='For computers with modern graphics cards'>HIGH</a>
-            <a className='quality-select' onClick={() => { this.initStage('low') }} tooltip='For Computers with low power graphics cards'>MEDIUM</a>
+            <a className='quality-select' onClick={() => { this.initStage('low') }} tooltip='For Computers with low power graphics cards'>MEDIUM</a> */}
           </div>
         </div>
       )
